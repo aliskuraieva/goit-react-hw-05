8c9276bdfc518c25a69df12e08103cd725599eda
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-10-23 18:44:45 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// import "modern-normalize";
 import "./index.css";
+import "modern-normalize";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
